202efc7219ef0c3e25e1c634ebc6b3e0820b88cc
Alexander Yuryatin <14263965+yuryatin@users.noreply.github.com>
2023-06-14 03:45:41 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from distutils.core import setup
 setup(
     name = 'egcst',
     packages = ['egcst'],
-    version = '0.0.2',
+    version = '0.0.3',
     license = 'MIT',
     description = 'This package performs triangulation for engineering geological cross-sections',
     url = 'https://github.com/yuryatin/egcst',
-    download_url = 'https://github.com/yuryatin/egcst/archive/refs/tags/v0.0.2.tar.gz',
+    download_url = 'https://github.com/yuryatin/egcst/archive/refs/tags/v0.0.3.tar.gz',
     keywords = ['engineering', 'geological', 'cross', 'sections', 'mapping', 'triangulation', 'delaunay'],
     classifiers = [],
     install_requires = ['numpy', 'matplotlib', 'shapely', 'ground', 'sect']
