021337ecf6fd0573a4ac2231fc355053ce64f1de
Alexander Yuryatin <14263965+yuryatin@users.noreply.github.com>
2023-06-15 20:32:28 +0300

    This version just adds import README to pypi

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 from distutils.core import setup
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
-    name = 'egcst',
-    packages = ['egcst'],
-    version = '0.0.8',
-    license = 'MIT',
-    description = 'This package performs triangulation for engineering geological cross-sections',
-    url = 'https://github.com/yuryatin/egcst',
-    download_url = 'https://github.com/yuryatin/egcst/archive/refs/tags/v0.0.8.tar.gz',
-    keywords = ['engineering', 'geological', 'cross', 'sections', 'mapping', 'triangulation', 'delaunay'],
-    classifiers = [],
-    install_requires = ['numpy', 'matplotlib', 'shapely', 'ground', 'sect']
+    name='egcst',
+    packages=['egcst'],
+    version='0.0.9',
+    license='MIT',
+    description='This package performs triangulation for engineering geological cross-sections',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/yuryatin/egcst',
+    download_url='https://github.com/yuryatin/egcst/archive/refs/tags/v0.0.9.tar.gz',
+    keywords=['engineering', 'geological', 'cross',
+              'sections', 'mapping', 'triangulation', 'delaunay'],
+    classifiers=[],
+    install_requires=['numpy', 'matplotlib', 'shapely', 'ground', 'sect']
 )
